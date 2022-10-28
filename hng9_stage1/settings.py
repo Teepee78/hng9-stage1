48166417858cf9080813daad735da8f3a96cc1b8
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-10-28 14:11:29 +0100

    Edit cors middleware

--- hng9_stage1/settings.py
+++ hng9_stage1/settings.py
@@ -50,9 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # CORS MiddleWare
     'corsheaders.middleware.CorsMiddleware',
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,5 +137,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ORIGIN_WHITELIST = ('*',)
