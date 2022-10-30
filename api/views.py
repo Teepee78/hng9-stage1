8d1eac670826094c3853efc0d53eccabd1e46f10
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-10-30 01:03:42 +0100

    update view

--- api/views.py
+++ api/views.py
@@ -3,12 +3,14 @@ from django.http import JsonResponse
 from rest_framework.generics import ListAPIView
 from .serializers import MyInfoSerializer
 from .models import MyInfo
+import json
 
 # Create your views here.
 def MyInfoView_func(request):
-    info = MyInfo.objects.all()
-    serializer = MyInfoSerializer(info)
-    return JsonResponse(serializer.data, safe=False)
+    # info = MyInfo.objects.all()
+    # serializer = MyInfoSerializer(info, many=True)
+    json_set = {"slackUsername": "Lase", "backend": True, "age": 23, "bio": "A Backend Engineer based in Lagos, Nigeria."}
+    return JsonResponse(json_set, safe=False)
 
 class MyInfoView(ListAPIView):
     queryset = MyInfo.objects.all()
